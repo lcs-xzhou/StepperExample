25630680e665bc3810dd2531a961343fdcf0f586
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-15 11:41:48 -0500

    Done the first step: creating the equation. Next step would be about creating the base and the buttons.

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+                
+                Text("5")
+                    .font(.system(size: 96))
+                
+                Text("2")
+                    .font(.system(size: 44))
+                
+                Text("=")
+                    .font(.system(size: 96))
+                
+                Text("25")
+                    .font(.system(size: 96))
+            }
+            
+            Spacer()
         }
         .padding()
     }
