2dbc28af753518d09a394273d935e5b68d1255f2
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-15 12:13:18 -0500

    Correct the mathematically incorrect statement of negative numbers.

---
+++ StepperExample/SumCalculator.swift
@@ -0,0 +1,18 @@
+//
+//  SumCalculator.swift
+//  StepperExample
+//
+//  Created by Fiona ZHOU on 2023-11-15.
+//
+
+import SwiftUI
+
+struct SumCalculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SumCalculator()
+}
